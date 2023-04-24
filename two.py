1e6c44cae38539cf725315b7a13c5a9bf54f1499
David Bartholomew <debartho@uno.edu>
2023-04-24 10:57:39 -0500

    liked changes

--- two.py
+++ two.py
@@ -5,3 +5,5 @@ guy = person("David",35)
 print("Person's name is", guy.get_name(),", and this person's age is", guy.get_age(),".")
 
 print("Made changes from other directory")
+
+#I like these changes!
