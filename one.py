1a056da6a5a7d959c7e744b0d9adff4791791c43
David Bartholomew <debartho@uno.edu>
2023-04-24 10:17:30 -0500

    changes to one

--- one.py
+++ one.py
@@ -1,1 +1,15 @@
+
+
 print("working one")
+
+class one:
+
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def get_name():
+		print(self.name)
+
+	def get_age():
+		print(self.age)
