5484e8b5bc2796cd8bad6ea04ba4dcd0648e1dbc
David Bartholomew <debartho@uno.edu>
2023-04-30 15:32:19 -0500

    nothing

--- two.py
+++
@@ -1,9 +0,0 @@
-from one import person
-
-guy = person("David",35)
-
-print("Person's name is", guy.get_name(),", and this person's age is", guy.get_age(),".")
-
-print("Made changes from other directory")
-
-#I like these changes!
